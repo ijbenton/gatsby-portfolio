98251ae26cdc28a23a22f4f62fe3bb4fe92e5cac
ijbenton <bentonian1@gmail.com>
2020-04-04 00:40:36 -0700

    new fav icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,13 +76,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `bentondev-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#028090`,
+        theme_color: `#028090`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bentondev-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
